73ee3a00918b55b76ab31ccba4002e4e9d29e12e
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-10-01 20:04:38 -0400

    Wrote code to sync sequelize models to database, then turn on server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+const runner = async () => {
+  try {
+   await sequelize.sync();
+   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+  } catch (err) {
+   console.error(err)
+  }
+ }
+ 
+ runner();
